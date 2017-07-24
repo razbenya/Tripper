af3e654e33925fe8fd2d33a7430bc88270232f3c
Raz Ben Yaish <razb13@gmail.com>
2017-07-24 17:14:40 +0300

    finish register and login

--- client/src/app/_services/authentication.service.ts
+++ client/src/app/_services/authentication.service.ts
@@ -8,8 +8,8 @@ export class AuthenticationService {
 
   constructor(private http: Http) { }
 
-  login(username: string, password: string) {
-      return this.http.post('/users/authenticate', { username: username, password: password })
+  login(email: string, password: string) {
+      return this.http.post('/users/authenticate', { email: email, password: password })
           .map((response: Response) => {
               // login successful if there's a jwt token in the response
               let user = response.json();
